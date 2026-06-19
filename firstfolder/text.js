0c9740c92ba9557eb7f8a5479d953f2a8045f237
Nikit kunwar <nikitkunwar456@gmail.com>
2026-06-19 16:30:48 +0000

    container

--- firstfolder/text.js
+++ firstfolder/text.js
@@ -1,1 +1,6 @@
-console.log("nikit");
+const accountId=1203
+console.log(accountId)
+let name= "NIkit kunwar"
+let address="butwal"
+console.log([accountId , name , address])
+console.table([accountId , name , address])
