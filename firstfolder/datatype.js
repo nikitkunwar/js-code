bca139014866d73025accf43cf156eebf250f596
Nikit kunwar <nikitkunwar456@gmail.com>
2026-06-20 07:05:05 +0000

    container

---
+++ firstfolder/datatype.js
@@ -0,0 +1,17 @@
+// console.log("33");
+// console.log(typeof "33");
+// let score="33"
+// let scoreInNumber=Number(score);
+// console.log(scoreInNumber)
+// console.log(typeof scoreInNumber)
+//let score="33abc"
+//let scoreInNumber=Number(score);
+// console.log(scoreInNumber)
+// console.log(typeof scoreInNumber)
+
+
+let score=""
+let scoreInBoolean=Boolean(score)
+console.log( typeof scoreInBoolean)
+console.log( scoreInBoolean)
+
